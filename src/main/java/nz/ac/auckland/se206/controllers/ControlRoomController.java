66bd57949eeb0d40af2aa565658a74f8c3ffd26b
arlene <asap990@aucklanduni.ac.nz>
2023-09-11 15:36:40 +1200

    add left and right arrows for travelling to rooms

--- src/main/java/nz/ac/auckland/se206/controllers/ControlRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ControlRoomController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
@@ -17,6 +18,10 @@ public class ControlRoomController {
   @FXML private Rectangle exitDoor;
   @FXML private Label timerLabel;
   @FXML private ProgressBar timerProgressBar;
+  @FXML private ImageView rightArrow;
+  @FXML private ImageView rightGlowArrow;
+  @FXML private ImageView leftArrow;
+  @FXML private ImageView leftGlowArrow;
 
   public void initialize() {
     // Initialization code goes here
@@ -57,5 +62,35 @@ public class ControlRoomController {
     System.out.println("keypad clicked");
 
     App.setRoot(AppUi.KEYPAD);
+  }
+
+  @FXML
+  public void onRightClicked(MouseEvent event) {
+    System.out.println("right arrow clicked");
+  }
+
+  @FXML
+  public void onRightHovered(MouseEvent event) {
+    rightGlowArrow.setVisible(true);
+  }
+
+  @FXML
+  public void onRightUnhovered(MouseEvent event) {
+    rightGlowArrow.setVisible(false);
+  }
+
+  @FXML
+  public void onLeftClicked(MouseEvent event) {
+    System.out.println("left arrow clicked");
+  }
+
+  @FXML
+  public void onLeftHovered(MouseEvent event) {
+    leftGlowArrow.setVisible(true);
+  }
+
+  @FXML
+  public void onLeftUnhovered(MouseEvent event) {
+    leftGlowArrow.setVisible(false);
   }
 }
