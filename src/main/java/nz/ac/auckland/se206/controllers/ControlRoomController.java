451adb39d6a06df1a71132075edbc4d5a278f36a
arlene <asap990@aucklanduni.ac.nz>
2023-09-15 15:32:11 +1200

    resolve merge conflicts in control room and lab controller

--- src/main/java/nz/ac/auckland/se206/controllers/ControlRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ControlRoomController.java
@@ -1,6 +1,9 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,7 +14,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager.AppUi;
@@ -35,6 +42,11 @@ public class ControlRoomController {
   @FXML private TextArea chatBox;
   @FXML private TextField messageBox;
   @FXML private Button sendMessage;
+  @FXML private ImageView character;
+  @FXML private ImageView running;
+  @FXML private Pane room;
+
+  private boolean moving = false;
 
   public void initialize() throws ApiProxyException {
     // Initialization code goes here
@@ -44,6 +56,24 @@ public class ControlRoomController {
     timerLabel.textProperty().bind(GameState.timerTask.messageProperty());
 
     chatBox.textProperty().bind(GameState.chatTextProperty());
+
+    // Set the initial position of the character within the Pane
+    character.setLayoutX(0); // Initial X position
+    character.setLayoutY(0); // Initial Y position
+
+    // Set the dimensions of the character
+    character.setFitWidth(150); // Width of character image
+    character.setFitHeight(150); // Height of character image
+
+    // Set the initial position of the running gif within the Pane
+    running.setLayoutX(0); // Initial X position
+    running.setLayoutY(0); // Initial Y position
+
+    // Set the dimensions of the running gif
+    running.setFitWidth(150); // Width of running gif
+    running.setFitHeight(150); // Height of running gif
+
+    RoomFramework.goToInstant(780, 480, character, running);
   }
 
   /**
@@ -184,5 +214,69 @@ public class ControlRoomController {
     GameState.appendChatMessage(msg);
 
     GameState.runGpt(msg);
+  }
+
+  /**
+   * 'Consumes' the mouse event, preventing it from being registered.
+   *
+   * @param event the mouse event
+   */
+  @FXML
+  public void consumeMouseEvent(MouseEvent event) {
+    System.out.println("mouse event consumed");
+    System.out.println(event.getSource());
+    event.consume();
+  }
+
+  /**
+   * Handles the mouse click event on the room, moving the character to the clicked location.
+   *
+   * @param event the mouse event
+   */
+  @FXML
+  public void onMoveCharacter(MouseEvent event) {
+    if (!moving) {
+
+      double mouseX = event.getX();
+      double mouseY = event.getY();
+
+      System.out.println("MouseX: " + mouseX + ", MouseY: " + mouseY);
+
+      // Create a circle for the click animation
+      Circle clickCircle = new Circle(5); // Adjust the radius as needed
+      clickCircle.setFill(Color.BLUE); // Set the color of the circle
+      clickCircle.setCenterX(mouseX);
+      clickCircle.setCenterY(mouseY);
+
+      // Add the circle to the room
+      room.getChildren().add(clickCircle);
+
+      // Create a fade transition for the circle
+      FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.4), clickCircle);
+      fadeOut.setFromValue(1.0);
+      fadeOut.setToValue(0.0);
+
+      // Create a scale transition for the circle
+      ScaleTransition scale = new ScaleTransition(Duration.seconds(0.4), clickCircle);
+      scale.setToX(3.0); // Adjust the scale factor as needed
+      scale.setToY(3.0); // Adjust the scale factor as needed
+
+      // Play both the fade and scale transitions in parallel
+      ParallelTransition parallelTransition = new ParallelTransition(fadeOut, scale);
+      parallelTransition.setOnFinished(
+          e -> {
+            // Remove the circle from the pane when the animation is done
+            room.getChildren().remove(clickCircle);
+          });
+
+      parallelTransition.play();
+      moving = true;
+      double movementDelay = RoomFramework.goTo(mouseX, mouseY, character, running);
+      Runnable resumeMoving =
+          () -> {
+            moving = false;
+          };
+      RoomFramework.delayRun(resumeMoving, movementDelay);
+    }
   }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/LabController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LabController.java
@@ -3,6 +3,9 @@ package nz.ac.auckland.se206.controllers;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,7 +15,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager.AppUi;
@@ -32,6 +39,13 @@ public class LabController {
   @FXML private TextArea chatBox;
   @FXML private TextField messageBox;
   @FXML private Button sendMessage;
+  @FXML private ImageView character;
+  @FXML private ImageView running;
+  @FXML private Pane room;
+
+  private boolean moving = false;
+  double startX = 1400;
+  double startY = 900;
 
   public void initialize() {
     // Initialization code goes here
@@ -41,6 +55,90 @@ public class LabController {
     timerLabel.textProperty().bind(GameState.timerTask.messageProperty());
 
     chatBox.textProperty().bind(GameState.chatTextProperty());
+
+    // Set the initial position of the character within the Pane
+    character.setLayoutX(0); // Initial X position
+    character.setLayoutY(0); // Initial Y position
+
+    // Set the dimensions of the character
+    character.setFitWidth(150); // Width of character image
+    character.setFitHeight(150); // Height of character image
+
+    // Set the initial position of the running gif within the Pane
+    running.setLayoutX(0); // Initial X position
+    running.setLayoutY(0); // Initial Y position
+
+    // Set the dimensions of the running gif
+    running.setFitWidth(150); // Width of running gif
+    running.setFitHeight(150); // Height of running gif
+
+    running.setScaleX(-1);
+    character.setScaleX(-1);
+    RoomFramework.goToInstant(startX, startY, character, running);
+  }
+
+  /**
+   * Handles the mouse click event on the room, moving the character to the clicked location.
+   *
+   * @param event the mouse event
+   */
+  @FXML
+  public void onMoveCharacter(MouseEvent event) {
+    if (!moving) {
+
+      double mouseX = event.getX();
+      double mouseY = event.getY();
+
+      System.out.println("MouseX: " + mouseX + ", MouseY: " + mouseY);
+
+      // Create a circle for the click animation
+      Circle clickCircle = new Circle(5); // Adjust the radius as needed
+      clickCircle.setFill(Color.BLUE); // Set the color of the circle
+      clickCircle.setCenterX(mouseX);
+      clickCircle.setCenterY(mouseY);
+
+      // Add the circle to the room
+      room.getChildren().add(clickCircle);
+
+      // Create a fade transition for the circle
+      FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.4), clickCircle);
+      fadeOut.setFromValue(1.0);
+      fadeOut.setToValue(0.0);
+
+      // Create a scale transition for the circle
+      ScaleTransition scale = new ScaleTransition(Duration.seconds(0.4), clickCircle);
+      scale.setToX(3.0); // Adjust the scale factor as needed
+      scale.setToY(3.0); // Adjust the scale factor as needed
+
+      // Play both the fade and scale transitions in parallel
+      ParallelTransition parallelTransition = new ParallelTransition(fadeOut, scale);
+      parallelTransition.setOnFinished(
+          e -> {
+            // Remove the circle from the pane when the animation is done
+            room.getChildren().remove(clickCircle);
+          });
+
+      parallelTransition.play();
+      moving = true;
+      double movementDelay = RoomFramework.goTo(mouseX, mouseY, character, running);
+      Runnable resumeMoving =
+          () -> {
+            moving = false;
+          };
+      RoomFramework.delayRun(resumeMoving, movementDelay);
+    }
+  }
+
+  /**
+   * 'Consumes' the mouse event, preventing it from being registered.
+   *
+   * @param event the mouse event
+   */
+  @FXML
+  public void consumeMouseEvent(MouseEvent event) {
+    System.out.println("mouse event consumed");
+    System.out.println(event.getSource());
+    event.consume();
   }
 
   @FXML
@@ -56,6 +154,24 @@ public class LabController {
     } catch (ApiProxyException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
+    }
+
+    consumeMouseEvent(event);
+
+    try {
+      if (!moving) {
+        double movementDelay = RoomFramework.goTo(660, 900, character, running);
+        Runnable goToPrinter =
+            () -> {
+              System.out.println("Printer clicked");
+              moving = false;
+            };
+
+        RoomFramework.delayRun(goToPrinter, movementDelay);
+      }
+    } catch (Exception e) {
+      // TODO handle exception appropriately
+      System.out.println("Error");
     }
   }
 
