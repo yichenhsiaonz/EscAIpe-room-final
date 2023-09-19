f1a392a824b05d56cf3c6b648dbf2a968acd1e0c
Albert <asun555@aucklanduni.ac.nz>
2023-09-19 19:56:17 +1200

    Added New Lab
    Redrew the lab and added it to the game. Redrawn lab uses the pixel art style which is consistent with the other rooms.

--- src/main/java/nz/ac/auckland/se206/controllers/LabController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LabController.java
@@ -39,8 +39,8 @@ public class LabController {
   @FXML private ImageView talkingAi;
 
   private boolean moving = false;
-  double startX = 1400;
-  double startY = 900;
+  double startX = 1300;
+  double startY = 630;
 
   public void initialize() {
     // Initialization code goes here
@@ -63,6 +63,8 @@ public class LabController {
 
       double mouseX = event.getX();
       double mouseY = event.getY();
+
+      System.out.println("Mouse clicked at: " + mouseX + ", " + mouseY);
 
       // Create a circle for the click animation
       Circle clickCircle = new Circle(5); // Adjust the radius as needed
@@ -120,7 +122,7 @@ public class LabController {
     try {
       if (!moving) {
         moving = true;
-        double movementDelay = GameState.goTo(660, 900, character, running);
+        double movementDelay = GameState.goTo(360, 680, character, running);
         Runnable goToPrinter =
             () -> {
               character.setOpacity(1);
