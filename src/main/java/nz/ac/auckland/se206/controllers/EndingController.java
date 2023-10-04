0198e0e8249030a434bf0c6171979e9105872a2c
Albert <asun555@aucklanduni.ac.nz>
2023-10-04 16:56:52 +1300

    USB Ending Logic
    Added boolean logic in the ending controller to play different ending depending on if the user has found the usb.

--- src/main/java/nz/ac/auckland/se206/controllers/EndingController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/EndingController.java
@@ -44,57 +44,61 @@ public class EndingController {
    */
   @FXML
   private void onNextClicked() throws ApiProxyException {
-    TextToSpeechManager.cutOff();
-    if (chatCount == 0) { // show neutral AI
-      textArea.clear();
-      Image newImage = new Image("/images/Ending/neutral-frame.png");
-      shadowFrame.setImage(newImage);
-      textArea.appendText("AI: " + GameState.endingCongrats);
-      TextToSpeechManager.speak(GameState.endingCongrats);
-    } else if (chatCount == 1) { // show evil AI
-      textArea.clear();
-      Image newImage = new Image("/images/Ending/evil-frame.png");
-      shadowFrame.setImage(newImage);
-      textArea.appendText("AI: " + GameState.endingReveal);
-      TextToSpeechManager.speak(GameState.endingReveal);
-    } else if (chatCount == 2) { // show win screen
-      // create black rectangle that covers the entire AnchorPane
-      AnchorPane anchorPane = (AnchorPane) nextButton.getParent();
-      AnchorPane blackRectangle = new AnchorPane();
-      blackRectangle.setStyle("-fx-background-color: black;");
-      blackRectangle.setOpacity(0.0);
-      AnchorPane.setTopAnchor(blackRectangle, 0.0);
-      AnchorPane.setBottomAnchor(blackRectangle, 0.0);
-      AnchorPane.setLeftAnchor(blackRectangle, 0.0);
-      AnchorPane.setRightAnchor(blackRectangle, 0.0);
+    if (!GameState.isUsbEnding) {
+      TextToSpeechManager.cutOff();
+      if (chatCount == 0) { // show neutral AI
+        textArea.clear();
+        Image newImage = new Image("/images/Ending/neutral-frame.png");
+        shadowFrame.setImage(newImage);
+        textArea.appendText("AI: " + GameState.endingCongrats);
+        TextToSpeechManager.speak(GameState.endingCongrats);
+      } else if (chatCount == 1) { // show evil AI
+        textArea.clear();
+        Image newImage = new Image("/images/Ending/evil-frame.png");
+        shadowFrame.setImage(newImage);
+        textArea.appendText("AI: " + GameState.endingReveal);
+        TextToSpeechManager.speak(GameState.endingReveal);
+      } else if (chatCount == 2) { // show win screen
+        // create black rectangle that covers the entire AnchorPane
+        AnchorPane anchorPane = (AnchorPane) nextButton.getParent();
+        AnchorPane blackRectangle = new AnchorPane();
+        blackRectangle.setStyle("-fx-background-color: black;");
+        blackRectangle.setOpacity(0.0);
+        AnchorPane.setTopAnchor(blackRectangle, 0.0);
+        AnchorPane.setBottomAnchor(blackRectangle, 0.0);
+        AnchorPane.setLeftAnchor(blackRectangle, 0.0);
+        AnchorPane.setRightAnchor(blackRectangle, 0.0);
 
-      anchorPane.getChildren().add(blackRectangle);
+        anchorPane.getChildren().add(blackRectangle);
 
-      // create fade transition
-      FadeTransition fadeToBlack = new FadeTransition(Duration.seconds(5), blackRectangle);
-      fadeToBlack.setFromValue(0.0);
-      fadeToBlack.setToValue(1.0);
+        // create fade transition
+        FadeTransition fadeToBlack = new FadeTransition(Duration.seconds(5), blackRectangle);
+        fadeToBlack.setFromValue(0.0);
+        fadeToBlack.setToValue(1.0);
 
-      // show win screen when the fade animation is complete
-      fadeToBlack.setOnFinished(
-          event -> {
-            // remove rectangle
-            anchorPane.getChildren().remove(blackRectangle);
+        // show win screen when the fade animation is complete
+        fadeToBlack.setOnFinished(
+            event -> {
+              // remove rectangle
+              anchorPane.getChildren().remove(blackRectangle);
 
-            // clear current screen
-            shadowFrame.setVisible(false);
-            textArea.setVisible(false);
-            nextButton.setVisible(false);
+              // clear current screen
+              shadowFrame.setVisible(false);
+              textArea.setVisible(false);
+              nextButton.setVisible(false);
 
-            // show win screen
-            winImage.setVisible(true);
-            menuButton.setVisible(true);
-          });
+              // show win screen
+              winImage.setVisible(true);
+              menuButton.setVisible(true);
+            });
 
-      fadeToBlack.play();
+        fadeToBlack.play();
+      }
+
+      chatCount++;
+    } else {
+      System.out.println("Play usb ending");
     }
-
-    chatCount++;
   }
 
   /** Returns to the main menu. */
